4ca0631e636c519bbdfff515ad25fdb3db8653bc
shimogamo <shimogamo84@yahoo.co.jp>
2020-02-07 01:24:22 +0900

    ゲームパッド

--- pad.py
+++ pad.py
@@ -13,11 +13,13 @@ class GamePad(object):
         logger.info(f'ポート使用：{port}')
         self._ser = Serial(port)
 
-    def press(self, cmd, duration1=.045, duration2=.035):
+    def press(self, cmd, duration1=.045, duration2=.035, show_log=False):
         self._ser.write(f'{cmd}\r\n'.encode('utf-8'))
         sleep(duration1)
         self._ser.write('RELEASE\r\n'.encode('utf-8'))
         sleep(duration2)
+        if show_log:
+            logger.info(f'{cmd}')
         return self
 
     def repeat(self, cmd, n, duration1=.045, duration2=.035):
